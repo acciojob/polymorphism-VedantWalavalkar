6679beb9d0636b557cff876dfcd5204410ee1546
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-09 13:16:39 +0530

    Method Overloaded 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class Main {
     static class Product{
-        Product(){
-        }
+//        Product(){
+//        }
 //        private int x, y, z;
         public int product(int x, int y){
 //            this.x = x;
